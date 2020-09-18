11cc99108301287c6beefcb5f1783dfeb53cf732
taeyeong <kty3749@gmail.com>
2020-09-18 16:55:20 +0900

    20200918-1655-검색슬라이드 제작, animation 추가, footer  작성

--- KTY/js/main.js
+++ KTY/js/main.js
@@ -1,19 +1,55 @@
-(function menuCloseFunction() {
-    const menuSlideBg = document.querySelector('.menuSlideBg');
-    const menuSlide = document.querySelector('.menuSlide');
-    const menuBtn = document.querySelector('.menuBtn');
-    const menuCloseBtn = document.querySelector('.menuCloseBtn');
+(function slideFunction() {
+  const slideBg = document.querySelector('.slideBg');
 
-    function createSlide() {
-        menuSlide.style.display = 'flex';
-        menuSlideBg.style.display = 'block';
-    }
-    function removeSlide(){
-        menuSlide.style.display = 'none';
-        menuSlideBg.style.display = 'none';
-    }
-    menuBtn.addEventListener('click',()=>createSlide());
-    menuCloseBtn.addEventListener('click',()=>removeSlide());
-    menuSlideBg.addEventListener('click',()=>removeSlide());
+  const menuSlide = document.querySelector('.menuSlide');
+  const menuBtn = document.querySelector('.menuBtn');
+  const menuCloseBtn = document.querySelector('.menuCloseBtn');
+
+  const searchSlide = document.querySelector('.searchSlide');
+  const searchBtn = document.querySelector('.searchBtn');
+  const searchCloseBtn = document.querySelector('.searchCloseBtn .directionIcon');
+
+  function createMenuSlide() {
+    menuSlide.style.transform = 'translateX(0)';
+    slideBg.style.visibility = 'visible';
+    slideBg.style.opacity = '1';
+  }
+  function removeSlide(){
+    menuSlide.style.transform = '';
+    slideBg.style.visibility = 'hidden';
+    slideBg.style.opacity = '0';
+    searchSlide.style.transform = '';
+  }
+  function createSearchSlide() {
+    searchSlide.style.transform = 'translateX(0)';
+    slideBg.style.visibility = 'visible';
+    slideBg.style.opacity = '1';
+  }
+
+  menuBtn.addEventListener('click',()=>createMenuSlide());
+  menuCloseBtn.addEventListener('click',()=>removeSlide());
+  slideBg.addEventListener('click',()=>removeSlide());
+
+  searchBtn.addEventListener('click',()=>createSearchSlide());
+  searchCloseBtn.addEventListener('click',()=>removeSlide());
 
 })();
+
+// (function searchInputDeleteFunction() {
+//   const searchInput = document.querySelector('.searchInput');
+//   const inputDeleteBtn = document.querySelector('.inputDeleteBtn');
+    
+//   function creatDeleteBtn() {
+//     inputDeleteBtn.style.display = 'block';
+//     console.log(1);
+//   }
+//   function removeDeleteBtn() {
+//     inputDeleteBtn.style.display = 'none';
+//     console.log(21);
+//   }
+//   searchInput.addEventListener('valid',()=>creatDeleteBtn());
+//   searchInput.addEventListener('invalid',()=>removeDeleteBtn());
+
+// //   inputDeleteBtn.addEventListener('click'),
+
+// })();
