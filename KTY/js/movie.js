071893ebdadbea9d94fd1633b1d8254b4e1b5e1f
taeyeong <kty3749@gmail.com>
2020-09-22 15:14:16 +0900

    20200922-1514-영화재생페이지 #hash마다 다른 화면 나오게 js 제작

---
+++ KTY/js/movie.js
@@ -0,0 +1,62 @@
+let movie = [
+  {
+    _id : 'mulan',
+    title : '뮬란',
+    grade : '8.32',
+    genre : '모험, 드라마, 가족',
+    time : '115',
+    rate : '12',
+    img : './images/mulan.jpg',
+    video : './videos/tenet.mp4',
+  },
+  {
+    _id : 'TheWomanWhoRan',
+    title : '도망친 여자',
+    grade : '8.23',
+    genre : '드라마',
+    time : '77',
+    rate : '18',
+    img : './images/TheWomanWhoRan.jpg',
+    video : './videos/tenet.mp4',
+  },
+  {
+    _id : 'tenet',
+    title : '테넷',
+    grade : '7.00',
+    genre : '액션, SF',
+    time : '110',
+    rate : '15',
+    img : './images/tenet.jpg',
+    video : './videos/tenet.mp4',
+  },
+]
+
+function hashCheck() {
+  if(location.hash){
+    const hash = location.hash.substring(1)
+    console.log(hash);
+    const mvIndex = movie.findIndex(x=>x._id===hash)
+    console.log(mvIndex);
+    const player = document.querySelector('.player');
+    const mvTitle = document.querySelector('.mvTitle');
+    const mvGrade = document.querySelector('.mvGrade');
+    const mvGenre = document.querySelector('.mvGenre');
+    const mvTime = document.querySelector('.mvTime');
+    const mvRate = document.querySelector('.mvRate');
+    
+    player.src = movie[mvIndex].video;
+    player.poster = movie[mvIndex].img;
+    mvTitle.innerHTML = movie[mvIndex].title;
+    mvGrade.innerHTML = `평점 ${movie[mvIndex].grade} /`;
+    mvGenre.innerHTML = `${movie[mvIndex].genre} /`;
+    mvTime.innerHTML = `${movie[mvIndex].time}분 /`;
+    mvRate.innerHTML = `${movie[mvIndex].rate}세 /`;
+  }
+
+    //없으면 첫번째 탭 버튼 활성화
+  else{
+    console.log(1);
+  }
+};
+
+hashCheck();
