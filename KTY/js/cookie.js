c0c1bf0be1a1f4a55b572c6f8f21053dc4a7952b
taeyeong <kty3749@gmail.com>
2020-09-23 17:01:56 +0900

    20200923-1701-로그아웃 방식 수정

--- KTY/js/cookie.js
+++ KTY/js/cookie.js
@@ -1,9 +1,9 @@
 (function userCookieCheck(){
   const menuLoginText = document.querySelector('.menuLoginText');
   const menuLoginTextH3 = document.querySelector('.menuLoginText h3');
-  const menuLoginTextDiv = document.querySelector('.menuLoginText div');
   const login = document.querySelector('.login');
   const signUp = document.querySelector('.signUp');
+  const logout = document.querySelector('.logout');
 
   let decodedCookie = decodeURIComponent(document.cookie);
   let cookiePiece = decodedCookie.split(';');
@@ -14,21 +14,16 @@
     }
     if (cookiePiece[i].indexOf('userId') == 0) {
       userId = cookiePiece[i].substring(7, cookiePiece[i].length);
-      userName = getID(userId).name.substring(1);
+      userName = getID(userId).name;
       
       menuLoginTextH3.style.display = 'none';
       login.style.display = 'none';
       signUp.style.display = 'none';
-      
+      logout.style.display = 'inline';
+
       let h2 = document.createElement('h2');
       h2.innerHTML = `안녕하세요 ${userName}님`;
       menuLoginText.prepend(h2);
-
-      let span = document.createElement('span');
-      span.className = 'logout';
-      span.innerHTML = '로그아웃';
-      menuLoginTextDiv.append(span);
-      
     }
   }
 
